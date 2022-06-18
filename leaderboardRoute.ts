c97648e438abc7d1185ea5dcbfca3d3c6342d518
Trent Shailer <trent.shailer@gmail.com>
2022-06-18 14:01:22 +1200

    Name validator

--- leaderboardRoute.ts
+++ leaderboardRoute.ts
@@ -1,5 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { v4 } from "uuid";
+import Filter from "bad-words";
+const filter = new Filter();
 
 interface ILeaderboardAddBody {
 	secret: string;
@@ -7,7 +9,48 @@ interface ILeaderboardAddBody {
 	score: number;
 }
 
+interface IValidNameQuery {
+	name: string;
+}
+
+interface NameValidator {
+	valid: boolean;
+	reason: string;
+}
+
+function IsValidChar(c: string): boolean {
+	if (!c.match("[a-zA-Z_-0-9]")) {
+		return false;
+	}
+	return true;
+}
+
+function ValidateName(name: string): NameValidator {
+	let nameValidator: NameValidator = { valid: true, reason: "" };
+
+	if (name === undefined || name === null || name === "" || name.length < 1) {
+		nameValidator.valid = false;
+		nameValidator.reason = "You must enter a name";
+	} else if (name.length > 16) {
+		nameValidator.valid = false;
+		nameValidator.reason = "Name is too long (max 16)";
+	} else if (filter.isProfane(name)) {
+		nameValidator.valid = false;
+		nameValidator.reason = "Name contains profanity";
+	} else if (name.match("[^a-zA-Z0-9_-]")) {
+		nameValidator.valid = false;
+		nameValidator.reason = "Name contains invalid character";
+	}
+	return nameValidator;
+}
+
 async function leaderboardRoutes(fastify: FastifyInstance, options: any) {
+	fastify.get<{ Querystring: IValidNameQuery }>("/valid-name", async (request, reply) => {
+		let name = request.query.name;
+
+		return reply.send(ValidateName(name));
+	});
+
 	fastify.get("/leaderboard", async (request, reply) => {
 		const client = await fastify.pg.connect();
 
